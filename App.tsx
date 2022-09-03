00cf666be13fd968231a0eeb8dfff221540c7614
doerme <398921432@qq.com>
2022-09-03 18:41:14 +0800

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import * as React from 'react';
+import { useRef, useEffect } from 'react';
+import { yyEva } from 'yyeva';
 import './style.css';
 
 export default function App() {
+  const playerContainer = useRef<HTMLDivElement>(null);
+  const playerRef = useRef(null);
+  useEffect(() => {
+    (async () => {
+      playerRef.current = await yyEva({
+        container: playerContainer.current, // Html Element
+        videoUrl:
+          'https://unpkg.yy.com/webupload/e-video/demo/AAA_dynamic_264_mid-0.mp4', // Video Url
+        // Events
+        onStart() {},
+        onStop() {},
+        onEnd() {},
+        onPause() {},
+        onResume() {},
+        onProcess() {},
+        onError() {},
+      });
+      // playerRef.current.start();
+    })();
+  }, []);
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>yyeva demo</h1>
+      <p>yyeva demo:</p>
+      <div ref={playerContainer}></div>
     </div>
   );
 }
